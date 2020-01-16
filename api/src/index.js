e8c8a52a33d83bb781329e7ffaf05e3872087fff
juanpetterson <juan.petterson@gmail.com>
2020-01-16 20:50:16 -0300

    Implemented backend for DevLocation Application

---
+++ api/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://juanpetterson:omnistack@cluster0-hfyur.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
