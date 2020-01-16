e97aec98774bf307eec578fb0a7638c240d4eecf
juanpetterson <juan.petterson@gmail.com>
2020-01-16 20:59:26 -0300

    Implemented Web frontend of DevLocation Application

---
+++ web/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleSubmit(data) {
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <div className="wrapper">
+          <strong>Cadastrar</strong>
+          <DevForm onSubmit={handleSubmit} />
+        </div>
+      </aside>
+
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem dev={dev} key={dev._id} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
